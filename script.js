6455f1783a0bf8d671386ed4bb98721364c25012
Soumyadeep Banerjee <87521742+SoumyadeepB2001@users.noreply.github.com>
2025-04-21 03:42:42 +0530

    Added regex to check for invalid ip address

--- script.js
+++ script.js
@@ -10,24 +10,45 @@ var coords = document.getElementById('coords');
 var org = document.getElementById('org');
 var timezone = document.getElementById('timezone');
 
-fetch('https://api.ipify.org/?format=json')  //fetching API to show current IP Address
-    .then(response => response.json())
-    .then(data => {
-        my_ip.innerHTML = "Your IP Address: " + data['ip'];
-    })
-button.addEventListener('click', function (name) {     //Submit button event listener to show info about the user specified IP Address
+// Regex for validating IP
+function isValidIP(ip) {
+    const ipv4 = /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3}$/;
+    const ipv6 = /^(([0-9a-fA-F]{1,4}):){7}([0-9a-fA-F]{1,4})$/;
+    return ipv4.test(ip) || ipv6.test(ip);
+}
+
+// Function to fetch and display IP info
+function fetchIPInfo(targetIP) {
     ip.innerHTML = "Fetching IP Address Information...";
-    fetch('https://ipinfo.io/' + input.value + '/geo')
+    fetch('https://ipinfo.io/' + targetIP + '/geo')
         .then(response => response.json())
         .then(data => {
-            ip.innerHTML = "IP Address: " + data['ip'];
-            city.innerHTML = "City: " + data['city'];
-            hostname.innerHTML = "Host Name: " + data['hostname'];
-            region.innerHTML = "Region: " + data['region'] + ", " + data['country'];
-            coords.innerHTML = "Co-ordinates: " + data['loc'];
-            org.innerHTML = "Organization: " + data['org'];
-            pin.innerHTML = "PIN Code: " + data['postal'];
-            timezone.innerHTML = "Timezone: " + data['timezone'];
+            ip.innerHTML = "IP Address: " + data.ip;
+            city.innerHTML = "City: " + data.city;
+            hostname.innerHTML = "Host Name: " + (data.hostname || "Unavailable");
+            region.innerHTML = "Region: " + data.region + ", " + data.country;
+            coords.innerHTML = "Co-ordinates: " + data.loc;
+            org.innerHTML = "Organization: " + data.org;
+            pin.innerHTML = "PIN Code: " + data.postal;
+            timezone.innerHTML = "Timezone: " + data.timezone;
         })
-        .catch(err => ip.innerHTML = "Wrong Input!");
-})
+        .catch(err => ip.innerHTML = "Error fetching IP info!");
+}
+
+// Get and display user's IP
+fetch('https://api.ipify.org/?format=json')
+    .then(response => response.json())
+    .then(data => {
+        my_ip.innerHTML = "Your IP Address: " + data.ip;
+        fetchIPInfo(data.ip);  // Fetch info about user's IP on load
+    });
+
+// Button click handler
+button.addEventListener('click', () => {
+    const userInput = input.value.trim();
+    if (isValidIP(userInput)) {
+        fetchIPInfo(userInput);
+    } else {
+        ip.innerHTML = "Invalid IP address format!";
+    }
+});
